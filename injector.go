94a37f8f73014c3d9bbbeaaf590b24cdb784fbeb
室岡 豊人 <toyohito.murooka@P0724-M.local>
2018-12-26 22:14:58 +0900

    wire

---
+++ injector.go
@@ -0,0 +1,68 @@
+//+build wireinject
+
+package main
+
+import (
+	"memo_sample/adapter/db"
+	"memo_sample/adapter/logger"
+	"memo_sample/adapter/memory"
+	view "memo_sample/adapter/view/render"
+	"memo_sample/interface/api"
+	"memo_sample/usecase"
+
+	"github.com/google/wire"
+)
+
+// WireInjectAPI inject api using wire
+var WireInjectAPI = wire.NewSet(
+	WireInjectUsecaseIterator,
+	api.NewAPI,
+)
+
+// WireInjectPresenter inject presenter using wire
+var WireInjectPresenter = wire.NewSet(
+	WireInjectRender,
+	WireInjectLog,
+	api.NewPresenter,
+)
+
+// WireInjectMemoUsecase inject memo usecase using wire
+var WireInjectMemoUsecase = wire.NewSet(
+	WireInjectDBRepository, // or WireInjectInMemoryRepository
+	usecase.NewMemo,
+)
+
+// WireInjectUsecaseIterator inject usecase itetator using wire
+var WireInjectUsecaseIterator = wire.NewSet(
+	WireInjectPresenter,
+	WireInjectMemoUsecase,
+	usecase.NewInteractor,
+)
+
+// WireInjectInMemoryRepository inject repository using wire
+var WireInjectInMemoryRepository = wire.NewSet(
+	memory.NewTransactionRepository,
+	memory.NewMemoRepository,
+	memory.NewTagRepository,
+)
+
+// WireInjectDBRepository inject repository using wire
+var WireInjectDBRepository = wire.NewSet(
+	db.NewTransactionRepository,
+	db.NewMemoRepository,
+	db.NewTagRepository,
+)
+
+// WireInjectLog inject log using wire
+var WireInjectLog = wire.NewSet(loggersub.NewLogger)
+
+// WireInjectRender inject render using wire
+var WireInjectRender = wire.NewSet(view.NewJSONRender)
+
+// InjectAPIServer build inject api using wire
+func InjectAPIServer() api.API {
+	wire.Build(
+		WireInjectAPI,
+	)
+	return nil
+}
